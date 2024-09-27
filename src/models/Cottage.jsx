2e5cf66a65bc13545c8d4084af667c2e71a51b4a
EVGENII <ovr58armee@yandex.ru>
2024-09-28 00:22:29 +0400

    cottage optimized

--- src/models/Cottage.jsx
+++ src/models/Cottage.jsx
@@ -59,7 +59,7 @@ const Cottage = ({coordinates, stage, isRotating, setIsRotating, angle, setAngle
     }
   }, [gl, handlePointerDown, handlePointerUp])
 
-  const { scene } = useMemo(() => useGLTF(cottageScene))
+  const { nodes, materials } = useMemo(() => useGLTF(cottageScene))
 
 
   useFrame(() => {
@@ -87,7 +87,634 @@ const Cottage = ({coordinates, stage, isRotating, setIsRotating, angle, setAngle
       <group ref={cameraTarget} position={[0, 9.5, 0]} />
       <group ref={cameraPosition} position-x={-30} position-y={15} position-z={30} />
       <RigidBody type='fixed' colliders='trimesh' >
-        <primitive object={scene} {...props} ref={cottageRef} />
+      <group {...props} dispose={null} ref={cottageRef}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balkon_beton_002.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_005.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_006.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_007.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_008.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Bassein001.geometry}
+          material={materials['Material #35']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Beton_003.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Bordur001.geometry}
+          material={materials['Material #29']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box076.geometry}
+          material={materials['Material #63']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box077.geometry}
+          material={materials['Material #63']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box078.geometry}
+          material={materials['Material #63']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box079.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box080.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box081.geometry}
+          material={materials['Material #29']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box082.geometry}
+          material={materials['Material #63']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box083.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box084.geometry}
+          material={materials['Material #81']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box085.geometry}
+          material={materials['Material #81']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box086.geometry}
+          material={materials['Material #81']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box087.geometry}
+          material={materials['Material #81']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box090.geometry}
+          material={materials['Material #82']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box091.geometry}
+          material={materials['Material #82']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box092.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box096.geometry}
+          material={materials['Material #3']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box097.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box098.geometry}
+          material={materials['Material #48']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box099.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box100.geometry}
+          material={materials['Material #48']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box101.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box102.geometry}
+          material={materials['Material #48']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box103.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box104.geometry}
+          material={materials['Material #48']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box105.geometry}
+          material={materials['Material #64']}
+          position={[-8.071, 0.643, -10.047]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box106.geometry}
+          material={materials['Material #114']}
+          position={[-8.091, 1.391, -10.039]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box107.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box108.geometry}
+          material={materials['Material #114']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box109.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box110.geometry}
+          material={materials['Material #48']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box111.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box112.geometry}
+          material={materials['Material #48']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box113.geometry}
+          material={materials['Material #64']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box114.geometry}
+          material={materials['Material #114']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cylinder005.geometry}
+          material={materials['Material #83']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cylinder006.geometry}
+          material={materials['Material #88']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box088.geometry}
+          material={materials['Material #85']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cylinder004.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Doroga001.geometry}
+          material={materials['Material #47']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Ruckha001.geometry}
+          material={materials['Material #3']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Vhodnaya_dver001.geometry}
+          material={materials['Material #62']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Garag001.geometry}
+          material={materials['Material #83']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Gazon001.geometry}
+          material={materials['Material #34']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Krysha001.geometry}
+          material={materials['Material #29']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object009.geometry}
+          material={materials['Material #82']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.kusty001.geometry}
+          material={materials['Material #48']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Lejak_003.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Lejak001.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object010.geometry}
+          material={materials['Material #3']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object011.geometry}
+          material={materials['Material #61']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object012.geometry}
+          material={materials['Material #2']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object013.geometry}
+          material={materials['Material #3']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object014.geometry}
+          material={materials['Material #61']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object015.geometry}
+          material={materials['Material #63']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Okna_1_etaj001.geometry}
+          material={materials['Material #3']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Okna_2_etaj001.geometry}
+          material={materials['Material #3']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Okna_steklo_2_etaj001.geometry}
+          material={materials['Material #61']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Perekritie_003.geometry}
+          material={materials['Material #65']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Perekritie_004.geometry}
+          material={materials['Material #65']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Perila_balkon001.geometry}
+          material={materials['Material #82']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Perila003.geometry}
+          material={materials['Material #82']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Perila004.geometry}
+          material={materials['Material #82']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Perila005.geometry}
+          material={materials['Material #82']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane013.geometry}
+          material={materials['Material #69']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane014.geometry}
+          material={materials['Material #88']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane015.geometry}
+          material={materials['Material #88']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane016.geometry}
+          material={materials['Material #88']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane017.geometry}
+          material={materials['Material #88']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane018.geometry}
+          material={materials['Material #88']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane019.geometry}
+          material={materials['Material #88']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane020.geometry}
+          material={materials['Material #88']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane021.geometry}
+          material={materials['Material #88']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane022.geometry}
+          material={materials['Material #82']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane023.geometry}
+          material={materials['Material #82']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane024.geometry}
+          material={materials['Material #88']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Plitka_pol001.geometry}
+          material={materials['Material #31']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Pol_2_etaj001.geometry}
+          material={materials['Material #69']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Steklo_Balkon_003.geometry}
+          material={materials['Material #61']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Steklo_balkon001.geometry}
+          material={materials['Material #61']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Steklo_okna_1_etaj001.geometry}
+          material={materials['Material #61']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Steny_1_etaj001.geometry}
+          material={materials['Material #2']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Steny_2_etaj001.geometry}
+          material={materials['Material #2']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Stol006.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Torus002.geometry}
+          material={materials['Material #115']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Box089.geometry}
+          material={materials['Material #82']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Плоскость.geometry}
+          material={materials.Материал}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Object014001.geometry}
+          material={materials['Material #61']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_007001.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_007002.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_007003.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_007004.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_007005.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_007006.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_007007.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_007008.geometry}
+          material={materials['Material #38']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Balky_007009.geometry}
+          material={materials['Material #38']}
+        />
+      </group>
       </RigidBody>
       {coordinates.map((point, i) => (
         <CapsuleCollider
