4f5c0f727ba6720b67b63619e4e6673d625f3ef9
EVGENII <ovr58armee@yandex.ru>
2024-09-27 16:05:02 +0400

    audio fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,6 @@ function Home() {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsPlaying(true)
       setIsRotating(true)
     }, 19000); // 3000 миллисекунд = 3 секунды
 
